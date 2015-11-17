75aa5d51a4d22573a66a554b147723053c4ba242
Thutmose <elpatricimo@gmail.com>
2015-11-17 18:00:41 -0400

    re-organized the samples a bit, removed one of the lua scrips from the zip

--- samples/exportsites.lua
+++ samples/exportsites.lua
@@ -1,6 +1,3 @@
--- On map load writes information about the loaded region to gamelog.txt
--- By Kurik Amudnil and Warmist (http://www.bay12forums.com/smf/index.php?topic=91166.msg4467072#msg4467072)
-
 local function write_gamelog(msg)
     local log = io.open('sites.txt', 'a')
     log:write(msg.."\n")
@@ -19,8 +16,6 @@ end
 
 local args = {...}
 
-print('Sites '..NEWLINE)
-
 local i = 1
 local num = 0
 local site = df.world_site.find(i)
@@ -29,29 +24,7 @@ local sized = false
 sized = site.global_min_x ~= site.global_max_x
 local message = i..''
 
---local details = df.world_data.rivers
-
---print(details)
-
---[[
-print(fullname(site)..' '..i)
-print(sized)
-print(site.buildings[2].unk3)
-print(site.pos.x..','..site.pos.y)
-print(site.global_min_x..','..site.global_min_y..'->'..site.global_max_x..','..site.global_max_y)
-if sized then
-	message = message..':'..site.global_min_x..','..site.global_min_y..'->'..site.global_max_x..','..site.global_max_y
-end
-print(message)
---]]
-
-----[[
 while not df.isnull(site) do
-	-- site positions
-	-- site  .pos.x  .pos.y
-	-- site  .rgn_min_x  .rgn_min_y  .rgn_max_x  .rgn_max.y
-	-- site  .global_min_x  .global_min_y  .global_max_x  .global_max_y
-	--site.name
 	sized = site.global_min_x ~= site.global_max_x
 	sized = true
 	
@@ -63,7 +36,7 @@ while not df.isnull(site) do
 	
 	i = i + 1
 	site = df.world_site.find(i)
-end --]]
+end
 
 print(num.." Sized sites")
 
